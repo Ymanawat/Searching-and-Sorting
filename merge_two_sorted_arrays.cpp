345b6a20982b0e508442dc83e98900e8db38529c
Yogendra Manawat <81252768+Ymanawat@users.noreply.github.com>
2022-05-28 01:47:55 +0530

    Update merge_two_sorted_arrays.cpp

--- merge_two_sorted_arrays.cpp
+++ merge_two_sorted_arrays.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #include<climits>
 void merge(int *arr1, int size1, int *arr2, int size2, int *ans)
 {
-    
     int i=0,j=0,k=0;
     
 	while (i<size1 && j<size2){
@@ -14,59 +13,11 @@ void merge(int *arr1, int size1, int *arr2, int size2, int *ans)
         }
         else{
             ans[k++]=arr2[j++];
-        }
-        
+	}     
     }
-    
-
 	while (i < size1)
 		ans[k++] = arr1[i++];
 
-
 	while (j < size2)
-		ans[k++] = arr2[j++];
-    
-}
-
-int main()
-{
-	int t;
-	cin >> t;
-	
-	while (t--)
-	{
-		int size1;
-		cin >> size1;
-
-		int *arr1 = new int[size1];
-
-		for (int i = 0; i < size1; i++)
-		{
-			cin >> arr1[i];
-		}
-
-		int size2;
-		cin >> size2;
-
-		int *arr2 = new int[size2];
-
-		for (int i = 0; i < size2; i++)
-		{
-			cin >> arr2[i];
-		}
-
-		int *ans = new int[size1 + size2];
-
-		merge(arr1, size1, arr2, size2, ans);
-
-		for (int i = 0; i < size1 + size2; i++)
-		{
-			cout << ans[i] << " ";
-		}
-
-		cout << endl;
-		delete[] arr1;
-		delete[] arr2;
-		delete[] ans;
-	}
+		ans[k++] = arr2[j++];    
 }
